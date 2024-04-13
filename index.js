a451125a45ed299a9e8207d8c5f89536c5e5a41e
Flex <85615389+flexywork327@users.noreply.github.com>
2024-04-13 18:27:35 +0000

    added a loop to redownload additional videos without restarting the app

--- index.js
+++ index.js
@@ -8,101 +8,131 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question("Enter the YouTube video URL: ", (url) => {
-  ytdl
-    .getInfo(url)
-    .then((info) => {
-      const videoFormats = info.formats.filter(
-        (format) => format.hasVideo && format.hasAudio
-      );
+function startDownload() {
+  rl.question("Enter the YouTube video URL: ", (url) => {
+    ytdl
+      .getInfo(url)
+      .then((info) => {
+        const videoFormats = info.formats.filter(
+          (format) => format.hasVideo && format.hasAudio
+        );
 
-      const qualityOptions = videoFormats.map(
-        (format) => `${format.qualityLabel} (${format.container})`
-      );
-      console.log("\nAvailable video qualities:");
-      qualityOptions.forEach((option, index) => {
-        console.log(`${index + 1}. ${option}`);
-      });
+        const qualityOptions = videoFormats.map(
+          (format) => `${format.qualityLabel} (${format.container})`
+        );
+        console.log("\nAvailable video qualities:");
+        qualityOptions.forEach((option, index) => {
+          console.log(`${index + 1}. ${option}`);
+        });
 
-      rl.question(
-        "\nEnter the desired quality option number: ",
-        (qualityOptionNumber) => {
-          const qualityOption = parseInt(qualityOptionNumber, 10) - 1;
-          if (qualityOption >= 0 && qualityOption < videoFormats.length) {
-            const format = videoFormats[qualityOption];
-            console.log(`\nTitle: ${info.videoDetails.title}`);
-            console.log(`Download started for ${format.qualityLabel}`);
+        rl.question(
+          "\nEnter the desired quality option number: ",
+          (qualityOptionNumber) => {
+            const qualityOption = parseInt(qualityOptionNumber, 10) - 1;
+            if (qualityOption >= 0 && qualityOption < videoFormats.length) {
+              const format = videoFormats[qualityOption];
+              console.log(`\nTitle: ${info.videoDetails.title}`);
+              console.log(`Download started for ${format.qualityLabel}`);
 
-            // Sanitize filename to avoid invalid characters
-            const sanitizedTitle = path
-              .parse(info.videoDetails.title)
-              .name.replace(/[^\w\s\.-]/g, "_");
-            const fileName = `${sanitizedTitle}.${format.container}`;
+              // Sanitize filename to avoid invalid characters
+              const sanitizedTitle = path
+                .parse(info.videoDetails.title)
+                .name.replace(/[^\w\s\.-]/g, "_");
+              const fileName = `${sanitizedTitle}.${format.container}`;
 
-            const scriptDir = path.dirname(require.main.filename); // Get script directory
-            const downloadsPath = path.join(scriptDir, "Downloads"); // Downloads folder path
-            const filePath = path.join(downloadsPath, fileName);
+              const scriptDir = path.dirname(require.main.filename); // Get script directory
+              const downloadsPath = path.join(scriptDir, "Downloads"); // Downloads folder path
+              const filePath = path.join(downloadsPath, fileName);
 
-            // Create Downloads folder with write permissions if it doesn't exist
-            if (!fs.existsSync(downloadsPath)) {
-              try {
-                fs.mkdirSync(downloadsPath, { recursive: true });
-                fs.chmodSync(downloadsPath, 0o755); // Set write permission for user, group, and others
-              } catch (err) {
-                console.error("Error creating Downloads folder:", err);
-                rl.close();
-                return;
-              }
+              // Create Downloads folder with write permissions if it doesn't exist
+              createDownloadsFolder(downloadsPath, () => {
+                console.log("File Path:", filePath);
+                downloadVideo(filePath, format, url);
+              });
+            } else {
+              console.error("Invalid quality option number!");
+              rl.close();
             }
-
-            console.log("File Path:", filePath);
-
-            downloadVideo(filePath, format, url);
-          } else {
-            console.error("Invalid quality option number!");
-            rl.close();
           }
-        }
-      );
-    })
-    .catch((err) => {
-      console.error("Error occurred:", err);
+        );
+      })
+      .catch((err) => {
+        console.error("Error occurred:", err.message);
+        rl.close();
+      });
+  });
+}
+
+function createDownloadsFolder(folderPath, callback) {
+  if (!fs.existsSync(folderPath)) {
+    try {
+      fs.mkdirSync(folderPath, { recursive: true });
+      fs.chmodSync(folderPath, 0o755); // Set write permission for user, group, and others
+      callback();
+    } catch (err) {
+      console.error("Error creating Downloads folder:", err.message);
       rl.close();
-    });
-});
+    }
+  } else {
+    callback();
+  }
+}
 
 function downloadVideo(filePath, format, url) {
   const downloadStream = ytdl(url, { format: format });
   const fileStream = fs.createWriteStream(filePath);
 
-  downloadStream.on("progress", (chunkLength, downloaded, total) => {
-    const percentage = Math.floor((downloaded / total) * 100); // Use Math.floor for whole numbers
+  let totalBytes = 0;
+  let downloadedBytes = 0;
+  let lastProgressUpdate = 0;
 
-    // Build a progress bar string
-    const progressBarLength = 100; // Adjust this for desired bar length
-    const completedChars = Math.round((percentage / 100) * progressBarLength);
-    const remainingChars = progressBarLength - completedChars;
+  downloadStream.on("response", (response) => {
+    totalBytes = parseInt(response.headers["content-length"], 10);
+  });
 
-    // Create progress bar visual representation
-    let progressBar = "[";
-    progressBar += "#".repeat(completedChars);
-    progressBar += "-".repeat(remainingChars);
-    progressBar += "]";
-
-    // Display progress bar and percentage
-    console.clear(); // Clear the console before each update
-    console.log(`Downloaded: ${percentage}% ${progressBar}`);
+  downloadStream.on("data", (chunk) => {
+    downloadedBytes += chunk.length;
+    const now = Date.now();
+    if (now - lastProgressUpdate > 500) {
+      updateProgressBar(downloadedBytes, totalBytes);
+      lastProgressUpdate = now;
+    }
   });
 
   downloadStream.pipe(fileStream);
 
   downloadStream.on("end", () => {
     console.log("\nDownload completed!");
-    rl.close();
+    rl.question("Do you want to download another video (y/n)? ", (answer) => {
+      if (answer.toLowerCase() === "y") {
+        startDownload();
+      } else {
+        console.log("Exiting app...");
+        rl.close();
+      }
+    });
   });
 
   downloadStream.on("error", (err) => {
-    console.error("Error occurred during download:", err);
+    console.error("Error occurred during download:", err.message);
     rl.close();
   });
 }
+
+function updateProgressBar(downloadedBytes, totalBytes) {
+  const percentage = Math.floor((downloadedBytes / totalBytes) * 100);
+
+  const progressBarLength = 100;
+  const completedChars = Math.round((percentage / 100) * progressBarLength);
+  const remainingChars = progressBarLength - completedChars;
+
+  let progressBar = "[";
+  progressBar += "#".repeat(completedChars);
+  progressBar += "-".repeat(remainingChars);
+  progressBar += "]";
+
+  console.clear();
+  console.log(`Downloaded: ${percentage}% ${progressBar}`);
+}
+
+startDownload();
